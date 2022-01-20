59f0295de4b4d733ee6f200af6d0d472650c4725
radu-popescu <radu.popescu@outlook.com>
2022-01-20 11:57:44 +0000

    ExecuteCommand, CommandController

---
+++ src/PostCommand/Controllers/CommandController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostCommand.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CommandController : ControllerBase
+    {
+        [HttpPost]
+        public void PostCommand(string command)
+        {
+            ExecuteCommand.CaptureCommandOutput(command);
+
+        }
+
+
+    }
+}
---
+++ src/PostCommand/ExecuteCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace PostCommand
+{
+    public static class ExecuteCommand 
+    {
+        public static string BaseProgram { get; } = "/bin/bash";
+        public static string ExecCmdOption { get; } = "-c";
+
+
+
+
+        public static void CaptureCommandOutput(string command)
+        {
+            var proc = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = BaseProgram,
+                    Arguments = $"{ExecCmdOption} \"{command}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+
+
+
+        }
+    }
+}
